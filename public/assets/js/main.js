1c99c5a9193711633e915074811c0f4cadf4e6fe
Vinyug <vincentguyomarch89@gmail.com>
2022-12-14 11:47:04 +0100

    menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,17 @@
 "use strict"
+
+// Navbar animée
+const btnNav = document.querySelector(`.responsive-nav-btn`);
+const listeNav = document.querySelector(`nav ul`);
+let imgBtn = document.querySelector(`.responsive-nav-btn img`);
+
+btnNav.addEventListener(`click`, () => {
+    listeNav.classList.toggle(`active-nav`);
+
+    if(imgBtn.src.includes(`menu`)){
+        imgBtn.src = `/public/assets/images/cross.png`;
+
+    } else {
+        imgBtn.src = `/public/assets/images/menu.png`;
+    }
+})
