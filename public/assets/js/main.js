9d36468ba54b17b0a92aa970c19761fa469fb6a1
Vinyug <vincentguyomarch89@gmail.com>
2022-12-14 14:12:30 +0100

    async from

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,9 +9,41 @@ btnNav.addEventListener(`click`, () => {
     listeNav.classList.toggle(`active-nav`);
 
     if(imgBtn.src.includes(`menu`)){
-        imgBtn.src = `/public/assets/images/cross.png`;
+        imgBtn.src = `assets/images/cross.png`;
 
     } else {
-        imgBtn.src = `/public/assets/images/menu.png`;
+        imgBtn.src = `assets/images/menu.png`;
     }
+})
+
+
+// remplacer Jquery par API FETCH
+$(function() {
+
+    $('#contact-form').button(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(result) {
+
+                if(result.isSuccess) {
+                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
+                    $('#contact-form')[0].reset();
+                } else {
+                    $("#firstname + .comments").html(result.firstnameError);
+                    $("#name + .comments").html(result.nameError);
+                    $("#email + .comments").html(result.emailError);
+                    $("#phone + .comments").html(result.phoneError);
+                    $("#message + .comments").html(result.messageError);
+                }
+            }
+        });
+    });
+
 })
